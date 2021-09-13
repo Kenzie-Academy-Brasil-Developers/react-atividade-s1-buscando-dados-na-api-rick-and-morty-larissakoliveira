a496e2285cca2df3cfdbcb75686ec36be8dd92cb
Larissa Oliveira <oliveir5uwm@gmail.com>
2021-09-13 20:50:54 -0300

    feat:colocando reticencias em nomes com mais de 15 caracteres

--- src/components/CharCard/index.jsx
+++ src/components/CharCard/index.jsx
@@ -6,16 +6,19 @@ const CharCard = ({ character }) => {
 
         character.status === 'Alive' ?
             <div className='alive'>
-                {/* {character.name.length < 15}? */}
+                {character.name.length < 15 ?
                 <h2>{character.name}</h2>
-                {/* :
-                <h2>{character.name.substr(0,12) + '...'}</h2> */}
+                :
+                <h2>{character.name.substr(0,12) + '...'}</h2>}
                 <h5>{character.species}</h5>
                 <img src={character.image} alt={''} />
             </div>
             :
             <div className='dead'>
+                {character.name.length < 15 ?
                 <h2>{character.name}</h2>
+                :
+                <h2>{character.name.substr(0,12) + '...'}</h2>}
                 <h5>{character.species}</h5>
                 <img src={character.image} alt={''} />
             </div>
